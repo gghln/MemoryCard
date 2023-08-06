1cae264922ec754bb787462780d2fabd6e8910c6
gghln <61983256+gghln@users.noreply.github.com>
2023-08-07 02:29:45 +0300

    Rendered Cards and Shuffle them on click

--- memory-card-game/src/components/card.jsx
+++ memory-card-game/src/components/card.jsx
@@ -4,40 +4,53 @@ import './styles/card.css'
 
 function CreateCard(props){
     const {
-        id,
         currentScore,
         setCurrentScore
     } = props
     const [isClicked,setIsClicked] = useState(false)
-    const [name,setName] = useState('')
-    const [icon,setIcon] = useState('')
+    const [Pokemons,setPokemons] = useState([])
+    let id;
 
     const handleClick = () => {
+        //Increase Score on Click
         if(!(isClicked)){
             setCurrentScore(currentScore + 1)
             setIsClicked(false)
         }else{
            return
         }
-   }
+        //Shuffle Array on Click
+        Pokemons.sort(() => Math.random() - 0.5)
+    }
 
+    //Fetch the 10 first pokemon from the APP
+    //Store them in state Array pokemons
     useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-        .then(response => response.json())
-        .then(name => setName(name.name))
-        .catch(error => console.error(error));
+        for(id=1;id<11;id++){
+            const requestName = fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`).then(response => response.json())
+            Promise.all([requestName])
+            .then(([pokemon]) => {
+                const pokeId = pokemon.id
+                const pokeName = pokemon.name
+                const pokeIcon = pokemon.sprites.front_default
+                setPokemons(current => [...current,{pokeId,pokeName,pokeIcon}])
+            })
+            .catch(error => console.error(error))
+        }    
+    },[])
 
-        fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-        .then(response => response.json())
-        .then(icon => setIcon(icon.sprites.front_default))
-        .catch(error => console.error(error));
-    },[id])
-
-    return(
-        <div className="card" onClick={handleClick}>
-            <pre>{name}</pre>
-            {icon ? <img src={icon}/> : 'Loading...'}
-        </div>
+    //Render a card for each item stored in Pokemons Array. Using array.map() method
+    return(   
+        <>
+        {Pokemons.map((pokemon,index) => {
+            return(
+                <div className='card' onClick={handleClick} key={index}>
+                    <p>{pokemon.pokeName}</p>
+                    <img src={pokemon.pokeIcon} alt={pokemon.pokeName} />
+                </div>
+            )
+        })}
+        </>       
     )
 }
 
--- memory-card-game/src/components/grid.jsx
+++ memory-card-game/src/components/grid.jsx
@@ -4,20 +4,10 @@ import './styles/grid.css'
 
 function DisplayGrid (props){
     const {currentScore, setCurrentScore} = props
+    
     return(
-        <div>
-            <div className="grid-container">
-                <CreateCard id={1} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
-                <CreateCard id={2} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
-                <CreateCard id={3} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
-                <CreateCard id={4} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
-                <CreateCard id={5} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
-                <CreateCard id={6} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
-                <CreateCard id={7} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
-                <CreateCard id={8} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
-                <CreateCard id={9} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
-                <CreateCard id={10} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
-            </div>
+        <div className="grid-container">
+            <CreateCard currentScore={currentScore} setCurrentScore={setCurrentScore}/>
         </div>
     )
 }
